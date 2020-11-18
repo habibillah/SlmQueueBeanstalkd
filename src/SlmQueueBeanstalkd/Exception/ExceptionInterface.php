3774e8dcc270e27a1231b53b47dc158cc8f82486
Habibillah Ali <habibillah@gmail.com>
2020-11-18 22:29:33 +0700

    remove unused files

--- src/SlmQueueBeanstalkd/Exception/ExceptionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SlmQueueBeanstalkd\Exception;
-
-use SlmQueue\Exception\ExceptionInterface as BaseExceptionInterface;
-
-interface ExceptionInterface extends BaseExceptionInterface
-{
-}
